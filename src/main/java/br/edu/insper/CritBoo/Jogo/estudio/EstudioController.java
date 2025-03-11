5864836b231902b26845baefa2b7da5285c4fca0
DanielJuhoKim <danieljk@al.insper.edu.br>
2025-03-11 17:41:57 -0300

    Estudio Service e Controller

---
+++ src/main/java/br/edu/insper/CritBoo/Jogo/estudio/EstudioController.java
@@ -0,0 +1,38 @@
+package br.edu.insper.CritBoo.Jogo.estudio;
+
+import br.edu.insper.CritBoo.Usuario.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+public class EstudioController {
+    @Autowired
+    private EstudioService estudioService;
+
+    @GetMapping("/estudio")
+    public HashMap<String, Estudio> getEstudios() {
+        return estudioService.getEstudios();
+    }
+
+    @GetMapping("/estudio/{nomeEstudio}")
+    public Estudio getEstudioEspecifico(@PathVariable String nomeEstudio) {
+        return estudioService.getEstudioEspecifico(nomeEstudio);
+    }
+
+    @PostMapping("/estudio")
+    public Estudio registrarEstudio(@RequestBody Estudio estudio) {
+        return estudioService.registrarEstudio(estudio);
+    }
+
+    @DeleteMapping("/estudio/{nomeEstudio}")
+    public Estudio deletarEstudio(@PathVariable String nomeEstudio) {
+        return estudioService.removerEstudio(nomeEstudio);
+    }
+
+    @PutMapping("/estudio/{nomeEstudio}")
+    public HashMap<String, Estudio> atualizarEstudio(@PathVariable String nomeEstudio, @RequestBody Estudio estudio) {
+        return estudioService.atualizarEstudio(nomeEstudio, estudio);
+    }
+}
---
+++ src/main/java/br/edu/insper/CritBoo/Jogo/estudio/EstudioService.java
@@ -0,0 +1,53 @@
+package br.edu.insper.CritBoo.Jogo.estudio;
+
+import br.edu.insper.CritBoo.Jogo.Jogo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Service
+public class EstudioService {
+    private HashMap<String, Estudio> estudios = new HashMap<>();
+
+    public HashMap<String, Estudio> getEstudios() {
+        return estudios;
+    }
+
+    public Estudio getEstudioEspecifico(String nomeEstudio) {
+        return estudios.get(nomeEstudio);
+    }
+
+    public Estudio registrarEstudio(Estudio estudioRecebido) {
+        if (estudioRecebido.getNomeEstudio() != null && (estudioRecebido.getNomeEstudio().equals("")) != true) {
+            estudios.put(estudioRecebido.getNomeEstudio(), estudioRecebido);
+            return estudioRecebido;
+        }
+        return null;
+    }
+
+    public Estudio removerEstudio(String nomeEstudio) {
+        Estudio encontraEstudio = estudios.get(nomeEstudio);
+        if (encontraEstudio != null) {
+            estudios.remove(nomeEstudio);
+        }
+        return encontraEstudio;
+    }
+
+    public HashMap<String, Estudio> atualizarEstudio(String nomeEstudio, Estudio novoEstudio) {
+        Estudio encontraEstudio = estudios.get(nomeEstudio);
+        HashMap<String, Estudio> resposta = new HashMap<>();
+
+        if (encontraEstudio != null) {
+            resposta.put("Antigo", encontraEstudio);
+            if (novoEstudio.getNomeEstudio() != null && (novoEstudio.getNomeEstudio().equals("")) != true) {
+                resposta.put("Novo", novoEstudio);
+                estudios.remove(nomeEstudio);
+                estudios.put(novoEstudio.getNomeEstudio(), novoEstudio);
+
+                return resposta;
+            }
+        }
+        return null;
+    }
+}
